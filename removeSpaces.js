ca05d866727e5baec6023bfa45f7a17920971dbb
James Mernin <jamespmernin@gmail.com>
2020-09-28 18:20:47 -0400

    Got the algorithm to work

--- removeSpaces.js
+++ removeSpaces.js
@@ -1,7 +1,7 @@
 //REMOVE SPACES
 
 const removeSpaces = str => {
-
+  return str.replace(/\s+/g, ''); // source: https://stackoverflow.com/questions/5963182/how-to-remove-spaces-from-a-string-using-javascript
 }
 
 console.log(removeSpaces("alphabet soup is green   "))
